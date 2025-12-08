b36a52a96bd01e98250b7d4ad2c7d8b8d33dfd3f
MonkeyDHoffy <jonekstoff@googlemail.com>
2025-12-09 00:14:58 +0100

    card-stack

--- src/app/gametable/gametable.ts
+++ src/app/gametable/gametable.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
+import { NgStyle, NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-gametable',
-  imports: [],
+  imports: [NgStyle, NgFor],
   templateUrl: './gametable.html',
-  styleUrl: './gametable.css',
+  styleUrl: './gametable.scss',
 })
 export class Gametable {
+
+  takeCardAnimation = false;
+
+  takeCard() {
+    this.takeCardAnimation = true;
+  }
 
 }
