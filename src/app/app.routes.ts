faf012e3bffd2934981a2fdab6b45d8be7212233
MonkeyDHoffy <jonekstoff@googlemail.com>
2025-12-08 20:32:20 +0100

    startScreen

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { StartScreen } from './start-screen/start-screen';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    { path: '', component: StartScreen }
+];
