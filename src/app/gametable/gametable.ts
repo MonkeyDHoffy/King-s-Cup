a2a0408a1e61fbd333464e05ae0214644f8f903b
MonkeyDHoffy <jonekstoff@googlemail.com>
2025-12-09 23:37:11 +0100

    game logic shuffle stack etc

--- src/app/gametable/gametable.ts
+++ src/app/gametable/gametable.ts
@@ -1,18 +1,43 @@
 import { Component } from '@angular/core';
-import { NgStyle, NgFor } from '@angular/common';
+import { CommonModule } from '@angular/common';
+import { GameModel } from '../../models/gamemodels';
 
 @Component({
   selector: 'app-gametable',
-  imports: [NgStyle, NgFor],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './gametable.html',
   styleUrl: './gametable.scss',
 })
 export class Gametable {
 
   takeCardAnimation = false;
+  game: GameModel;
+  currentCard: string | undefined = '';
+
+  constructor() {
+    this.game = new GameModel();
+  }
+
+  ngOnInit(): void {
+    this.newGame();
+  }
+
+  newGame() {
+    this.game = new GameModel();
+    console.log('New game started', this.game);
+  }
 
   takeCard() {
+    if(!this.takeCardAnimation){
+    this.currentCard = this.game.stack.pop();
+    console.log('Card taken:', this.currentCard);
     this.takeCardAnimation = true;
+    setTimeout(() => {
+      this.takeCardAnimation = false;     
+    }, 1000);
   }
+}
+
 
 }
---
+++ src/models/gamemodels.ts
@@ -0,0 +1,34 @@
+export class GameModel {
+    public players:string[] = [];
+    public stack:string[] = [];
+    public playedCards:string[] = [];
+    public currentPlayer:number = 0;
+
+
+constructor() {
+    for (let i = 1; i < 14; i++) {
+    this.stack.push(`ace_${i}`);
+    this.stack.push(`hearts_${i}`);
+    this.stack.push(`diamonds_${i}`);
+    this.stack.push(`clubs_${i}`);
+}
+
+shuffleArray(this.stack);
+
+
+
+}
+
+  
+
+
+
+
+}
+
+  function shuffleArray(stack: any[]) {
+    for (let i = stack.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [stack[i], stack[j]] = [stack[j], stack[i]];
+    }
+}
